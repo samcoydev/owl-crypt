ba29732c9ff7374128c879d67f2b2415786cd50d
samcoydev <samuelcoydev@gmail.com>
2024-06-13 14:51:37 -0500

    Modified the starting room in 'the_crypt' to use a key

--- core/stock/dungeons/the_crypt/rooms/starter_room.py
+++ core/stock/dungeons/the_crypt/rooms/starter_room.py
@@ -1,12 +1,17 @@
-import core.stock.components.room_base as rb
-import core.stock.components.doorway_base as db
+import stock.components.base.room_base as rb
+import stock.components.base.doorway_base as db
+import stock.components.key_door as kd
 import core.base.types.direction_type as dt
+from base.objects.artifact import Artifact
+from stock.items.item_artifact import ItemArtifact
+from stock.items.key_base import KeyBase
 
 
 class StarterRoom(rb.RoomBase):
 
     def __init__(self):
         super().__init__(room_name="The Entrance", room_coordinates=(0, 0))
+        self.init_artifacts()
 
     @property
     def inspect_string(self) -> str:
@@ -15,9 +20,20 @@ class StarterRoom(rb.RoomBase):
                 "make sense of them.")
 
     def get_first_visit_text(self) -> str:
-        return ("\n\nYou notice a __Door on the East side of the room__. You can see a faint light coming from the other side.")
+        return (
+            "\n\nYou notice a __Door on the East side of the room__. You can see a faint light coming from the other side.")
 
     def init_doorways(self) -> None:
-        self.add_doorway(db.DoorwayBase(direction=dt.Direction.EAST,
-                                        inspect_string="The doorway is made up of large wooden planks, with torch "
-                                                       "light breaking through the cracks."))
+        self.add_doorway(
+            kd.KeyDoor(
+                dt.Direction.EAST,
+                "The doorway is made up of large wooden planks, with torch light breaking through the cracks.",
+                "rusty_key"
+            )
+        )
+
+    def init_artifacts(self) -> None:
+        rusty_key = KeyBase("Rusty Key", "rusty_key", "It's a key. It feels fragile.")
+        artifact = ItemArtifact(rusty_key, "rusty_key")
+
+        self.artifacts.append(artifact)
