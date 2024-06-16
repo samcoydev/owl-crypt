808ebca4de2d69737780dddfb9c3cc344a5b7b7d
samuelcoy <samuelcoydev@gmail.com>
2024-06-16 16:22:47 -0500

    Converted test_player_actor.py to use pytest and fixed the give test

--- tests/unit/test_player_actor.py
+++ tests/unit/test_player_actor.py
@@ -1,93 +1,85 @@
-import unittest
-from unittest.mock import Mock, patch
-
-from flask import Flask
-from flask_socketio import SocketIO
+import pytest
 
 from core.base.objects.item import Item
-from core.engine.engine import Engine
 from tests.utils.dungeon_test_utils import DungeonTestUtils
 from tests.utils.user_utils import create_and_ready_users
 
 
-class PlayerActorTest(unittest.TestCase):
+@pytest.fixture
+def dungeon(game_engine, mock_emit):
+    dungeon = DungeonTestUtils.create_test_dungeon("Test Dungeon")
+    dungeon.setup_dungeon()
+    game_engine.game_manager.set_dungeon(dungeon)
 
-    @classmethod
-    @patch('flask_socketio.emit')
-    def setUpClass(cls, mock_emit) -> None:
-        mock_socket_io = Mock()
-        cls.game_engine = Engine()
-
-    @patch('flask_socketio.emit')
-    def setUp(self, mock_emit) -> None:
-        self.mock_emit = mock_emit
-        self.mock_emit.return_value = None
-
-        self.manager = self.game_engine.game_manager
-
-        self.dungeon = DungeonTestUtils.create_test_dungeon("Test Dungeon")
-        self.dungeon.setup_dungeon()
-        self.manager.set_dungeon(self.dungeon)
-
-        self._users = create_and_ready_users(2, self.manager)
-        self.user_a = self._users[0]
-        self.user_b = self._users[1]
-        self.player_a = self.user_a.player_actor
-        self.player_b = self.user_b.player_actor
-
-    def helper_give_item(self, item_name, key, player):
-        item = Item(item_name, item_name, "test desc")
-        player.add_to_inventory(key, item)
-
-    def helper_fill_inventory(self, item_name, key, player):
-        player.max_inventory_size = 1
-        self.helper_give_item(item_name, key, player)
-
-    def test_give_item_success(self):
-        item_name = "Key"
-        target_name = self.player_b.character.character_name
-        success_str = f"* You gave {target_name} {item_name}"
-
-        self.helper_give_item(item_name, "test_key", self.player_a)
-        self.assertEqual(len(self.player_a.inventory), 1)
-        self.assertEqual(self.player_a.inventory["test_key"].name, item_name)
-        self.assertEqual(len(self.player_b.inventory), 0)
-
-        result = self.player_a.give_item("test_key", target_name)
-        self.assertEqual(result, success_str)
-        self.assertEqual(len(self.player_a.inventory), 0)
-        self.assertEqual(len(self.player_b.inventory), 1)
-        self.assertEqual(self.player_b.inventory["test_key"].name, item_name)
-
-    def test_give_item_couldnt_give(self):
-        item_name = "Key"
-        target_name = self.player_b.character.character_name
-        couldnt_give_str = f"* You couldn't give {target_name} {item_name}"
-
-        self.helper_fill_inventory("Filler Key", "test_filler_key", self.player_b)
-        self.helper_give_item(item_name, "test_key", self.player_a)
-
-        self.assertEqual(len(self.player_a.inventory), 1)
-        self.assertEqual(len(self.player_b.inventory), 1)
-        self.assertEqual(self.player_b.max_inventory_size, 1)
-
-        result = self.player_a.give_item("test_key", target_name)
-        self.assertEqual(result, couldnt_give_str)
-        self.assertEqual(len(self.player_a.inventory), 1)
-        self.assertEqual(len(self.player_b.inventory), 1)
-
-    def test_give_item_fail(self):
-        target_name = self.player_b.character.character_name
-        fail_str = "You don't have that item."
-
-        self.assertEqual(len(self.player_a.inventory), 0)
-        self.assertEqual(len(self.player_b.inventory), 0)
-
-        result = self.player_a.give_item("test_key", target_name)
-        self.assertEqual(result, fail_str)
-        self.assertEqual(len(self.player_a.inventory), 0)
-        self.assertEqual(len(self.player_b.inventory), 0)
+    return dungeon
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture
+def test_users(dungeon, game_engine, mock_emit, persistence):
+    _users = create_and_ready_users(2, game_engine.game_manager)
+    game_engine.game_manager.start_game()
+    print(_users)
+    player_a = _users[0].player_actor
+    player_b = _users[1].player_actor
+    yield player_a, player_b
+
+
+def helper_give_item(item_name, key, player):
+    item = Item(item_name, item_name, "test desc")
+    player.add_to_inventory(key, item)
+
+
+def helper_fill_inventory(item_name, key, player):
+    player.max_inventory_size = 1
+    helper_give_item(item_name, key, player)
+
+
+def test_give_item_success(test_users):
+    player_a, player_b = test_users
+    item_name = "Key"
+    target_name = player_b.character.character_name
+    success_str = f"* You gave {target_name} {item_name}"
+
+    helper_give_item(item_name, "test_key", player_a)
+    assert len(player_a.inventory) == 1
+    assert player_a.inventory["test_key"].name == item_name
+    assert len(player_b.inventory) == 0
+
+    result = player_a.give_item("test_key", target_name)
+    assert result == success_str
+    assert len(player_a.inventory) == 0
+    assert len(player_b.inventory) == 1
+    assert player_b.inventory["test_key"].name == item_name
+
+
+def test_give_item_couldnt_give(test_users):
+    player_a, player_b = test_users
+    item_name = "Key"
+    target_name = player_b.character.character_name
+    couldnt_give_str = f"* You couldn't give {target_name} {item_name}"
+
+    helper_fill_inventory("Filler Key", "test_filler_key", player_b)
+    helper_give_item(item_name, "test_key", player_a)
+
+    assert len(player_a.inventory) == 1
+    assert len(player_b.inventory) == 1
+    assert player_b.max_inventory_size == 1
+
+    result = player_a.give_item("test_key", target_name)
+    assert result == couldnt_give_str
+    assert len(player_a.inventory) == 1
+    assert len(player_b.inventory) == 1
+
+
+def test_give_item_fail(test_users):
+    player_a, player_b = test_users
+    target_name = player_b.character.character_name
+    fail_str = "You don't have that item."
+
+    assert len(player_a.inventory) == 0
+    assert len(player_b.inventory) == 0
+
+    result = player_a.give_item("test_key", target_name)
+    assert result == fail_str
+    assert len(player_a.inventory) == 0
+    assert len(player_b.inventory) == 0
