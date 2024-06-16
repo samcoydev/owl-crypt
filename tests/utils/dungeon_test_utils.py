41bbb7f4561223344982d4960a3cc829687a0e6d
samuelcoy <samuelcoydev@gmail.com>
2024-06-16 12:05:07 -0500

    Fixed imports and old class instantiation

--- tests/utils/dungeon_test_utils.py
+++ tests/utils/dungeon_test_utils.py
@@ -1,8 +1,8 @@
-import stock.components.base.dungeon_base as dungeon_base
-import stock.components.base.room_base as room_base
-import stock.components.base.doorway_base as doorway_base
+import core.stock.components.base.dungeon_base as dungeon_base
+import core.stock.components.base.room_base as room_base
+import core.stock.components.base.doorway_base as doorway_base
 import core.base.types.direction_type as direction_type
-from base.types.enemy_type import EnemyType
+from core.base.types.enemy_type import EnemyType
 
 
 class DungeonTestUtils:
@@ -11,16 +11,16 @@ class DungeonTestUtils:
         class TestDungeon(dungeon_base.DungeonBase):
             def setup_dungeon(self):
                 # Define the setup logic for the test dungeon
-                self.add_room(DungeonTestUtils.create_test_rooms()[0](self))
-                self.add_room(DungeonTestUtils.create_test_rooms()[1](self))
+                self.add_room(DungeonTestUtils.create_test_rooms()[0]())
+                self.add_room(DungeonTestUtils.create_test_rooms()[1]())
 
         return TestDungeon(dungeon_name)
 
     @staticmethod
     def create_test_rooms():
         class TestRoomA(room_base.RoomBase):
-            def __init__(self, dungeon: 'dungeon_base.DungeonBase'):
-                super().__init__(room_name="Test Room A", room_coordinates=(0, 0), dungeon=dungeon)
+            def __init__(self):
+                super().__init__(room_name="Test Room A", room_coordinates=(0, 0))
 
             def init_doorways(self) -> None:
                 self.add_doorway(doorway_base.DoorwayBase(direction_type.Direction.NORTH, "Wooden Door"))
@@ -29,8 +29,8 @@ class DungeonTestUtils:
                 return "This is test room A"
 
         class TestRoomB(room_base.RoomBase):
-            def __init__(self, dungeon: 'dungeon_base.DungeonBase'):
-                super().__init__(room_name="Test Room B", room_coordinates=(0, 1), dungeon=dungeon)
+            def __init__(self):
+                super().__init__(room_name="Test Room B", room_coordinates=(0, 1))
 
             def init_doorways(self) -> None:
                 self.add_doorway(doorway_base.DoorwayBase(direction_type.Direction.SOUTH, "Wooden Door"))
