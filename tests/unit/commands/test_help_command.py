2aac617c2dbd04c2f530f3c1d50e45b16ac3e8ac
samcoydev <samuelcoydev@gmail.com>
2024-06-20 23:52:59 -0500

    Fixed test failure due to user being a function

--- tests/unit/commands/test_help_command.py
+++ tests/unit/commands/test_help_command.py
@@ -4,21 +4,21 @@ from core.engine.command_registry import command_registry
 from tests.utils.user_utils import create_test_user_and_add_to_game
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def user(game_engine):
     return create_test_user_and_add_to_game("test_interpreter_user", game_engine.game_manager)
 
 
-def test_help(game_engine):
+def test_help(game_engine, user):
     result = game_engine.command_interpreter.interpret("help", user)
     assert result != "Unknown command"
 
 
-def test_help_detailed(game_engine):
+def test_help_detailed(game_engine, user):
     result = game_engine.command_interpreter.interpret("help help", user)
     assert result == command_registry["help"].get_detailed_help_string()
 
 
-def test_help_tutorial(game_engine):
+def test_help_tutorial(game_engine, user):
     result = game_engine.command_interpreter.interpret("help tutorial", user)
     assert result == command_registry["help"].get_tutorial_str()
